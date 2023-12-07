1b6fd1d752940ea6bc0b5118d767dce987de9534
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-08 00:49:01 +0700

    Update db_blanja.js

--- src/config/db_blanja.js
+++ src/config/db_blanja.js
@@ -1,18 +1,19 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+});
+
 // const { Pool } = require("pg");
+// require("dotenv").config();
+
 // const pool = new Pool({
-//   user: process.env.PGUSER,
-//   password: process.env.PASSWORD,
-//   host: process.env.PGHOST,
-//   port: process.env.PGPORT,
-//   database: process.env.PGDATABASE,
+//   connectionString: "postgres://default:uczaPF5NWZf0@ep-restless-scene-28667671-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
 // });
 
-const { Pool } = require("pg");
-require("dotenv").config();
-
-const pool = new Pool({
-  connectionString: "postgres://default:uczaPF5NWZf0@ep-restless-scene-28667671-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
-});
 pool.connect((err) => {
   if (err) throw err;
   console.log("Connect to PostgreSQL Successfully!");
