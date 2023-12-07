a9098f262e543b309400e3363b1a1a472e5b071b
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-08 05:40:44 +0700

    add protect

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const productsControllers = require("../controllers/products");
-// const { protect } = require("../middlewares/auth");
+const { protect } = require("../middlewares/auth");
 const upload = require("../middlewares/upload");
 
 router.get("/", productsControllers.getAllProduct);
 router.get("/:id", productsControllers.getProduct);
-router.post("/", upload, productsControllers.insertProduct);
-router.put("/:id", upload, productsControllers.updateProduct);
-router.delete("/:id", productsControllers.deleteProduct);
+router.post("/", protect, upload, productsControllers.insertProduct);
+router.put("/:id", protect, upload, productsControllers.updateProduct);
+router.delete("/:id", protect, productsControllers.deleteProduct);
 
 module.exports = router;
