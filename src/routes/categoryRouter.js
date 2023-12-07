75cd56b6b1b1e3b1af8cccc3affdd6b2bc2a1594
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-08 05:39:58 +0700

    Update categoryRouter.js

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middlewares/upload");
 const categoryControllers = require("../controllers/category");
-const { protect } = require("../middlewares/auth");
+// const { protect } = require("../middlewares/auth");
 
 router.get("/", categoryControllers.getAllCategory);
 router.get("/:id", categoryControllers.getCategory);
-router.post("/", protect, upload, categoryControllers.insertCategory);
-router.put("/:id", protect, upload, categoryControllers.updateCategory);
-router.delete("/:id", protect, categoryControllers.deleteCategory);
+router.post("/", upload, categoryControllers.insertCategory);
+router.put("/:id", upload, categoryControllers.updateCategory);
+router.delete("/:id", categoryControllers.deleteCategory);
 
 module.exports = router;
