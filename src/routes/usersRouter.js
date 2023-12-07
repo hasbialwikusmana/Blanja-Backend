dc55ff2005515f0cab011e49fc5a547be060808b
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-08 00:49:22 +0700

    Update usersRouter.js

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// const upload = require("../middlewares/upload");
 const usersControllers = require("../controllers/usersControllers");
 
 router
-  .get("/users", usersControllers.user)
-  // .get("/profile", usersControllers.profile)
-  // .get("/:id", usersControllers.getselectUsers)
-  // .put("/profile/:id", upload, usersControllers.updateImg)
-  // .put("/customer/:id", usersControllers.updateCustInfo)
+
   .post("/auth/register-customer", usersControllers.registerCustomer)
   .post("/auth/register-seller", usersControllers.registerSeller)
   .post("/auth/login", usersControllers.login)
