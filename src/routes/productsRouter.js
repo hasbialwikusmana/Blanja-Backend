11d1fcd76523eb2fc04acbc0f6d670b4a32f0156
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-12 17:18:43 +0700

    update validasi

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const productsControllers = require("../controllers/products");
-const { protect } = require("../middlewares/auth");
+const { protect, isSeller } = require("../middlewares/auth");
 const upload = require("../middlewares/upload");
 
 router.get("/", productsControllers.getAllProduct);
 router.get("/:id", productsControllers.getProduct);
 router.get("/category/:id", productsControllers.getCategory);
-router.post("/", protect, upload, productsControllers.insertProduct);
+router.post("/", protect, isSeller, upload, productsControllers.insertProduct);
 router.put("/:id", protect, upload, productsControllers.updateProduct);
 router.delete("/:id", protect, productsControllers.deleteProduct);
 
