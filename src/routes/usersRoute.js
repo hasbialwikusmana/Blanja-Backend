8b130c88e4505c66aa78eba722bab94ecb4d2d63
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-12 17:18:31 +0700

    Update usersRoute.js

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middlewares/upload");
 const usersControllers = require("../controllers/users");
-const { protect } = require("../middlewares/auth");
+const { protect, isCustomer, isSeller } = require("../middlewares/auth");
 
 router.get("/", usersControllers.user);
 router.get("/customer", usersControllers.customer);
 router.get("/seller", usersControllers.seller);
 router.get("/profile", protect, usersControllers.profile);
 router.get("/:id", usersControllers.getselectUsers);
-router.put("/profile", protect, upload, usersControllers.updatePhoto);
+router.put("/profile/:id", protect, upload, usersControllers.updatePhoto);
+router.put("/customer/profile", protect, isCustomer, usersControllers.updateProfileCustomer);
+router.put("/seller/profile", protect, isSeller, usersControllers.updateProfileSeller);
 router.put("/:id", usersControllers.updateUsers);
 router.delete("/:id", usersControllers.deleteUsers);
 
