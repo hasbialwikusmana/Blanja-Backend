9e81c9e682590e51064c3fe53a183b463192f4d9
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-19 12:35:12 +0700

    Update common.js

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -1,16 +1,14 @@
 const response = (res, result, status, message, pagination) => {
   const resultPrint = {};
-  resultPrint.status = "Success";
+  resultPrint.status = "success";
   if (status >= 400) {
-    resultPrint.status = "Error";
+    resultPrint.status = "error";
   }
-  resultPrint.status_code = status || 200;
-  resultPrint.data = result || null;
+  resultPrint.statusCode = status;
+  resultPrint.data = result;
   resultPrint.message = message || null;
-  resultPrint.pagination = pagination;
-  return res.status(resultPrint.status_code).json(resultPrint);
+  resultPrint.pagination = pagination || {};
+  res.status(status).json(resultPrint);
 };
 
-module.exports = {
-  response,
-};
+module.exports = { response };
